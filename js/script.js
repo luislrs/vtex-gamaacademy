46372587b51e6ed3b1906aa33493358e95ee6d32
luislrs <luislirs@outlook.com>
2021-07-18 05:34:34 -0300

    landing page black friday

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const form = document.getElementById('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let data = {
+        nome,
+        email,
+    }
+    let convertData = JSON.stringify(data);
+
+    localStorage.setItem('lead', convertData)
+
+    let content = document.getElementById('content')
+
+    let carregando = `<img src="assets/reload.gif">`
+
+    let ok = `<p>Cadastrado com sucesso</p>`
+
+    content.innerHTML = carregando
+
+    setTimeout(() => {
+        content.innerHTML = ok
+    }, 2000)
+})
